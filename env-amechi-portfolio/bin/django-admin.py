3d9302cd9b942e61a1969c4b3f9f016e7339e03e
amechi101 <egbe@usc.edu>
2015-07-29 15:46:09 -0400

    fresh commit

---
+++ env-amechi-portfolio/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/amechiegbe/Desktop/cm_dev/portfolio/env-amechi-portfolio/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
